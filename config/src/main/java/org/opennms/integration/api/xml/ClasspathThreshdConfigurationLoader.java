1db35cb67dbd51e11ec860a1d2945ca8096e505f
Matthew Brooks <matt@opennms.com>
2019-08-29 15:32:14 -0400

    OIA-14: Extend Threshold configuration

---
+++ config/src/main/java/org/opennms/integration/api/xml/ClasspathThreshdConfigurationLoader.java
@@ -0,0 +1,257 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2019 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2019 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.integration.api.xml;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.opennms.integration.api.v1.config.thresholding.ExcludeRange;
+import org.opennms.integration.api.v1.config.thresholding.Filter;
+import org.opennms.integration.api.v1.config.thresholding.IncludeRange;
+import org.opennms.integration.api.v1.config.thresholding.PackageDefinition;
+import org.opennms.integration.api.v1.config.thresholding.Parameter;
+import org.opennms.integration.api.v1.config.thresholding.Service;
+import org.opennms.integration.api.v1.config.thresholding.ServiceStatus;
+import org.opennms.integration.api.v1.config.thresholding.ThresholderDefinition;
+import org.opennms.integration.api.xml.schema.thresholding.Package;
+import org.opennms.integration.api.xml.schema.thresholding.ThreshdConfiguration;
+import org.opennms.integration.api.xml.schema.thresholding.Thresholder;
+
+/**
+ * Used to load XML threshd configuration from the class-path.
+ *
+ * @author mbrooks
+ * @since 1.0.0
+ */
+public class ClasspathThreshdConfigurationLoader extends ClasspathXmlLoader<ThreshdConfiguration> {
+    public ClasspathThreshdConfigurationLoader(Class<?> clazz, String... fileNames) {
+        super(clazz, ThreshdConfiguration.class, "thresholding", fileNames);
+    }
+
+    public Integer getThreads() {
+        // Return only the largest of the threads elements
+        return getObjects()
+                .stream()
+                .map(ThreshdConfiguration::getThreads)
+                .filter(Objects::nonNull)
+                .max(Integer::compareTo)
+                .orElse(null);
+    }
+
+    public List<PackageDefinition> getPackages() {
+        return getObjects().stream()
+                .flatMap(tc -> tc.getPackages().stream())
+                .map(ClasspathThreshdConfigurationLoader::toPackageDefinition)
+                .collect(Collectors.toList());
+    }
+
+    public List<ThresholderDefinition> getThresholders() {
+        return getObjects().stream()
+                .flatMap(tc -> tc.getThresholders().stream())
+                .map(ClasspathThreshdConfigurationLoader::toThresholderDefinition)
+                .collect(Collectors.toList());
+    }
+
+    private static PackageDefinition toPackageDefinition(Package packageToConvert) {
+        return new PackageDefinition() {
+            private final Filter filter = toFilter(packageToConvert.getFilter());
+            private final List<String> specifics = Collections.unmodifiableList(packageToConvert.getSpecifics());
+            private final List<IncludeRange> includeRanges =
+                    Collections.unmodifiableList(packageToConvert.getIncludeRanges()
+                            .stream()
+                            .map(ClasspathThreshdConfigurationLoader::toIncludeRange)
+                            .collect(Collectors.toList()));
+            private final List<ExcludeRange> excludeRanges =
+                    Collections.unmodifiableList(packageToConvert.getExcludeRanges()
+                            .stream()
+                            .map(ClasspathThreshdConfigurationLoader::toExcludeRange)
+                            .collect(Collectors.toList()));
+            private final List<String> includedUrls = Collections.unmodifiableList(packageToConvert.getIncludeUrls());
+            private final List<String> outageCalendars =
+                    Collections.unmodifiableList(packageToConvert.getOutageCalendars());
+            private final List<Service> services = Collections.unmodifiableList(packageToConvert.getServices()
+                    .stream()
+                    .map(ClasspathThreshdConfigurationLoader::toService)
+                    .collect(Collectors.toList()));
+
+            @Override
+            public String getName() {
+                return packageToConvert.getName();
+            }
+
+            @Override
+            public Filter getFilter() {
+                return filter;
+            }
+
+            @Override
+            public List<String> getSpecifics() {
+                return specifics;
+            }
+
+            @Override
+            public List<IncludeRange> getIncludeRanges() {
+                return includeRanges;
+            }
+
+            @Override
+            public List<ExcludeRange> getExcludeRanges() {
+                return excludeRanges;
+            }
+
+            @Override
+            public List<String> getIncludeUrls() {
+                return includedUrls;
+            }
+
+            @Override
+            public List<Service> getServices() {
+                return services;
+            }
+
+            @Override
+            public List<String> getOutageCalendars() {
+                return outageCalendars;
+            }
+        };
+    }
+
+    private static ThresholderDefinition toThresholderDefinition(Thresholder thresholder) {
+        return new ThresholderDefinition() {
+            private final List<Parameter> parameters = Collections.unmodifiableList(thresholder.getParameters()
+                    .stream()
+                    .map(ClasspathThreshdConfigurationLoader::toParameter)
+                    .collect(Collectors.toList()));
+
+            @Override
+            public String getService() {
+                return thresholder.getService();
+            }
+
+            @Override
+            public String getClassName() {
+                return thresholder.getClassName();
+            }
+
+            @Override
+            public List<Parameter> getParameters() {
+                return parameters;
+            }
+        };
+    }
+
+    private static Filter toFilter(org.opennms.integration.api.xml.schema.thresholding.Filter filter) {
+        return new Filter() {
+            @Override
+            public Optional<String> getContent() {
+                return filter.getContent();
+            }
+        };
+    }
+
+    private static IncludeRange toIncludeRange(org.opennms.integration.api.xml.schema.thresholding.IncludeRange includeRange) {
+        return new IncludeRange() {
+            @Override
+            public String getBegin() {
+                return includeRange.getBegin();
+            }
+
+            @Override
+            public String getEnd() {
+                return includeRange.getEnd();
+            }
+        };
+    }
+
+    private static ExcludeRange toExcludeRange(org.opennms.integration.api.xml.schema.thresholding.ExcludeRange excludeRange) {
+        return new ExcludeRange() {
+            @Override
+            public String getBegin() {
+                return excludeRange.getBegin();
+            }
+
+            @Override
+            public String getEnd() {
+                return excludeRange.getEnd();
+            }
+        };
+    }
+
+    private static Service toService(org.opennms.integration.api.xml.schema.thresholding.Service service) {
+        return new Service() {
+            private final List<Parameter> parameters = Collections.unmodifiableList(service.getParameters()
+                    .stream()
+                    .map(ClasspathThreshdConfigurationLoader::toParameter)
+                    .collect(Collectors.toList()));
+
+            @Override
+            public String getName() {
+                return service.getName();
+            }
+
+            @Override
+            public Long getInterval() {
+                return service.getInterval();
+            }
+
+            @Override
+            public Boolean getUserDefined() {
+                return service.getUserDefined();
+            }
+
+            @Override
+            public Optional<ServiceStatus> getStatus() {
+                return service.getStatus().map(s -> ServiceStatus.valueOf(s.name()));
+            }
+
+            @Override
+            public List<Parameter> getParameters() {
+                return parameters;
+            }
+        };
+    }
+
+    private static Parameter toParameter(org.opennms.integration.api.xml.schema.thresholding.Parameter parameter) {
+        return new Parameter() {
+            @Override
+            public String getKey() {
+                return parameter.getKey();
+            }
+
+            @Override
+            public String getValue() {
+                return parameter.getValue();
+            }
+        };
+    }
+}
---
+++ config/src/main/java/org/opennms/integration/api/xml/ClasspathThresholdingConfigLoader.java
@@ -0,0 +1,214 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2019 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2019 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.integration.api.xml;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.opennms.integration.api.v1.config.thresholding.Basethresholddef;
+import org.opennms.integration.api.v1.config.thresholding.Expression;
+import org.opennms.integration.api.v1.config.thresholding.FilterOperator;
+import org.opennms.integration.api.v1.config.thresholding.GroupDefinition;
+import org.opennms.integration.api.v1.config.thresholding.ResourceFilter;
+import org.opennms.integration.api.v1.config.thresholding.Threshold;
+import org.opennms.integration.api.v1.config.thresholding.ThresholdType;
+import org.opennms.integration.api.xml.schema.thresholding.Group;
+import org.opennms.integration.api.xml.schema.thresholding.ThresholdingConfig;
+
+/**
+ * Used to load XML thresholding configuration from the class-path.
+ *
+ * @author mbrooks
+ * @since 1.0.0
+ */
+public class ClasspathThresholdingConfigLoader extends ClasspathXmlLoader<ThresholdingConfig> {
+    public ClasspathThresholdingConfigLoader(Class<?> clazz, String... fileNames) {
+        super(clazz, ThresholdingConfig.class, "thresholding", fileNames);
+    }
+
+    public List<GroupDefinition> getGroupDefinitions() {
+        return getObjects().stream()
+                .flatMap(tc -> tc.getGroups().stream())
+                .map(ClasspathThresholdingConfigLoader::toGroupDefinition)
+                .collect(Collectors.toList());
+    }
+
+    private static GroupDefinition toGroupDefinition(Group group) {
+        return new GroupDefinition() {
+            private final String name = group.getName();
+            private final String rrdRepository = group.getRrdRepository();
+            private final List<Threshold> thresholds = Collections.unmodifiableList(group.getThresholds()
+                    .stream()
+                    .map(ThresholdImpl::new)
+                    .collect(Collectors.toList()));
+            private final List<Expression> expressions = Collections.unmodifiableList(group.getExpressions()
+                    .stream()
+                    .map(ExpressionImpl::new)
+                    .collect(Collectors.toList()));
+
+            @Override
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public String getRrdRepository() {
+                return rrdRepository;
+            }
+
+            @Override
+            public List<Threshold> getThresholds() {
+                return thresholds;
+            }
+
+            @Override
+            public List<Expression> getExpressions() {
+                return expressions;
+            }
+        };
+    }
+
+    private static class BasethresholddefImpl implements Basethresholddef {
+        private final org.opennms.integration.api.xml.schema.thresholding.Basethresholddef basethresholddef;
+        private final List<ResourceFilter> resourceFilters;
+
+        BasethresholddefImpl(org.opennms.integration.api.xml.schema.thresholding.Basethresholddef basethresholddef) {
+            this.basethresholddef = Objects.requireNonNull(basethresholddef);
+            resourceFilters = Collections.unmodifiableList(basethresholddef.getResourceFilters()
+                    .stream()
+                    .map(ClasspathThresholdingConfigLoader::toResourceFilter)
+                    .collect(Collectors.toList()));
+        }
+
+        @Override
+        public Boolean getRelaxed() {
+            return basethresholddef.getRelaxed();
+        }
+
+        @Override
+        public Optional<String> getDescription() {
+            return basethresholddef.getDescription();
+        }
+
+        @Override
+        public ThresholdType getType() {
+            return ThresholdType.forName(basethresholddef.getType().getEnumName());
+        }
+
+        @Override
+        public String getDsType() {
+            return basethresholddef.getDsType();
+        }
+
+        @Override
+        public Double getValue() {
+            return basethresholddef.getValue();
+        }
+
+        @Override
+        public Double getRearm() {
+            return basethresholddef.getRearm();
+        }
+
+        @Override
+        public Integer getTrigger() {
+            return basethresholddef.getTrigger();
+        }
+
+        @Override
+        public Optional<String> getDsLabel() {
+            return basethresholddef.getDsLabel();
+        }
+
+        @Override
+        public Optional<String> getTriggeredUEI() {
+            return basethresholddef.getTriggeredUEI();
+        }
+
+        @Override
+        public Optional<String> getRearmedUEI() {
+            return basethresholddef.getRearmedUEI();
+        }
+
+        @Override
+        public FilterOperator getFilterOperator() {
+            return FilterOperator.valueOf(basethresholddef.getFilterOperator().name());
+        }
+
+        @Override
+        public List<ResourceFilter> getResourceFilters() {
+            return resourceFilters;
+        }
+    }
+
+    private static class ThresholdImpl extends BasethresholddefImpl implements Threshold {
+        private final String dsName;
+
+        ThresholdImpl(org.opennms.integration.api.xml.schema.thresholding.Threshold threshold) {
+            super(Objects.requireNonNull(threshold));
+            dsName = threshold.getDsName();
+        }
+
+        @Override
+        public String getDsName() {
+            return dsName;
+        }
+    }
+
+    private static class ExpressionImpl extends BasethresholddefImpl implements Expression {
+        private final String expression;
+
+        ExpressionImpl(org.opennms.integration.api.xml.schema.thresholding.Expression expression) {
+            super(Objects.requireNonNull(expression));
+            this.expression = expression.getExpression();
+        }
+
+        @Override
+        public String getExpression() {
+            return expression;
+        }
+    }
+
+    private static ResourceFilter toResourceFilter(org.opennms.integration.api.xml.schema.thresholding.ResourceFilter resourceFilter) {
+        return new ResourceFilter() {
+            @Override
+            public Optional<String> getContent() {
+                return resourceFilter.getContent();
+            }
+
+            @Override
+            public String getField() {
+                return resourceFilter.getField();
+            }
+        };
+    }
+}
