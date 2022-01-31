1300e0fed02bd8badb6a0cee184f00c043c40bbe
yang <yali@opennms.com>
2022-01-31 17:24:12 -0500

    recreate the vue component

---
+++ sample/src/main/resources/web/Sample.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <h1>This is test extension html</h1>
+    <img src="assets/opennms.png" alt="Sample">
+</div>
+</template>
+
+<script>
+  export default {
+    name: "SampleComponent"
+  }
+</script>
