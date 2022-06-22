5f6dae77a4ef78008e971a8ebb83c7ad3d49d7ff
mikewrosey <mrose@nanthealth.com>
2022-06-22 17:14:08 -0400

    support vue-router in plugin test app

--- ui-extension-test/src/main.ts
+++ ui-extension-test/src/main.ts
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import * as Vue from 'vue/dist/vue.esm-bundler'
+import * as Pinia from 'pinia'
+import externalComponent from './utils/externalComponent'
+import Router from './router'
 
-createApp(App).use(createPinia()).mount('#app')
+(window as any)['VRouter'] = Router;
+(window as any).Vue = Vue;
+(window as any).Pinia = Pinia;
+
+await externalComponent('http://localhost:5002/uiextension.es.js')
+
+createApp(App)
+  .use(createPinia())
+  .use(Router)
+  .mount('#app')
 
