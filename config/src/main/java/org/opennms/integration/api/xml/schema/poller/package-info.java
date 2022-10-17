222f1339d25a91a1ae7ad99f1949fd003b3c5495
Dmitri Herdt <dmitri@herdt.online>
2022-10-17 10:43:53 +0200

    OIA 46: copy OIA 46 functionality from master to release-1.x

---
+++ config/src/main/java/org/opennms/integration/api/xml/schema/poller/package-info.java
@@ -0,0 +1,12 @@
+@XmlSchema(
+           namespace = "http://xmlns.opennms.org/xsd/config/poller/api",
+           elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
+           xmlns={
+                   @XmlNs(prefix="", namespaceURI="http://xmlns.opennms.org/xsd/eventconf")
+           }
+        )
+package org.opennms.integration.api.xml.schema.poller;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
+
